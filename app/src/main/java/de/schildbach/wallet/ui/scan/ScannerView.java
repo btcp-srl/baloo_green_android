2108617a1b349fa3f7a9e25fb8b091895565bf2d
Stefano <stefano.fontana.2000@gmail.com>
2020-07-06 16:42:11 +0200

    rebrand application, change main package name to ti.baloo.bitcoinpeople.wallet

--- app/src/main/java/de/schildbach/wallet/ui/scan/ScannerView.java
+++ app/src/main/java/de/schildbach/wallet/ui/scan/ScannerView.java
@@ -30,7 +30,7 @@ import android.util.AttributeSet;
 import android.view.View;
 
 import com.google.zxing.ResultPoint;
-import it.bitcoinpeople.wallet.ui.R;
+import it.baloo.bitcoinpeople.wallet.ui.R;
 
 import java.util.HashMap;
 import java.util.Iterator;
